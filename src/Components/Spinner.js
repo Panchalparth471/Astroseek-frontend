6dab8ae254e60553e730c97469ffe6ca31f8610f
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2024-05-03 16:53:15 +0530

    Update Spinner.js

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import "./Spinner.css";
 
 function Spinner() {
@@ -11,18 +11,5 @@ function Spinner() {
     )
 }
 
-=======
-import "./Spinner.css";
 
-function Spinner() {
-    return (
-        <div className="w-full h-[calc(91vh-70px)]  flex items-center justify-center">
-
-
-            <div className='spinner'></div>
-        </div>
-    )
-}
-
->>>>>>> ba41696de80d4608382a85a0a71fea55627f0be9
 export default Spinner;
