5a8414b183241c367732d5d24cf4df5647daff25
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2024-05-03 16:54:12 +0530

    Update AppContext.js

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { createContext } from "react";
 import { useState } from "react";
 
@@ -25,31 +25,4 @@ export default function AppContextProvider({ children })
          {children}
      </AppContext.Provider>
 }
-=======
-import { createContext } from "react";
-import { useState } from "react";
 
-export const AppContext = createContext();
-
-export default function AppContextProvider({ children })
-{
-    const [formData, setFormData] = useState({ firstName: "", lastName: "", email: "", password: "", accountType: "Person" });
-    const [savedUser, setSavedUser] = useState(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [name, setName] = useState("");
-    const value = {
-        formData,
-        setFormData,
-        savedUser,
-        setSavedUser,
-        isLoggedIn,
-        setIsLoggedIn,
-        name,
-        setName
-    }
-
-     return <AppContext.Provider value={value}>
-         {children}
-     </AppContext.Provider>
-}
->>>>>>> ba41696de80d4608382a85a0a71fea55627f0be9
