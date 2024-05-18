9ad451f307a44f2cf19df666b906bd9cc4a36ee9
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2024-05-18 12:57:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import AppContextProvider from "./Context/AppContext";
+import { AppContextProvider } from "./Context/AppContext";
 import { ToastContainer } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
